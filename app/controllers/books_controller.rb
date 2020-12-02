7c5f18f893e4faa29df67522133d45905b61a972
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2020-12-02 14:05:47 +0000

    update3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,8 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @new_book = Book.new
+    @book_comment = BookComment.new
+    @book_comments = BookComment.all
   end
 
   def index
