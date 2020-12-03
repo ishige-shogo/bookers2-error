5cc8bbd6a264b406da97418affc37727271ad7e7
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2020-12-03 02:59:54 +0000

    start4

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
 
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+
 
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
