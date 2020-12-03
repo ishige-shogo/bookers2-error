2161ea9fc7721823ca9ad454068e821ee9154982
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2020-12-03 00:53:01 +0000

    finish3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,6 @@ class BooksController < ApplicationController
     @user = @book.user
     @new_book = Book.new
     @book_comment = BookComment.new
-    @book_comments = @book.book_comments
   end
 
   def index
