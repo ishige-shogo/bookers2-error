7ed311cbc00309b39d7f11bea14d196ef4757299
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2020-12-12 13:18:59 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 2020_12_09_003721) do
     t.datetime "updated_at", null: false
   end
 
+
+
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
